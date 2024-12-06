9528badefc73cb814539401b0ad294d1dae79cc5
Pramit726 <pramitde726@gmail.com>
2024-12-06 13:22:03 +0530

    feat(setup): add setup.py for project packaging and installation instructions

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Pramit De",
+    author_email="pramitde726@gmail.com",
+    packages=find_packages()
+)
